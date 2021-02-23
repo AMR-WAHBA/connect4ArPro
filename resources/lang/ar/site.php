0fe2695b618b840d8dc3aae7d1742def6c74c7a2
Mando1987 <mandoooman@gmail.com>
2021-02-23 20:34:04 +0200

    added lang files

---
+++ resources/lang/ar/site.php
@@ -0,0 +1,354 @@
+<?php
+
+
+return [
+
+  'add'                               => 'إضافه',
+  'next'                              => 'التالى',
+  'back'                              => 'السابق',
+  'search'                            => 'ابحث',
+  'edit'                              => 'تعديل',
+  'delete'                            => 'حذف',
+  'print'                             => 'طباعة',
+  'show'                              => 'عرض',
+  'login'                             => 'دخول',
+  'logout'                            => 'تسجيل خروج',
+  'added'                             => 'تم الإضافه بنجاح',
+  'edited'                            => 'تم التعديل بنجاح',
+  'deleted'                           => 'تم الحذف بنجاح',
+  'actions'                           => 'خيارات',
+  'datatable_empty'                   => 'لا يوجد أى بيانات حاليا ',
+  'datatable_zero_records'            => 'لا توجد بيانات مطابقة للكلمة التي تبحث عنها',
+  'empty_admin_add'                   => 'إضافة مدير جديد',
+  'active'                            => 'يعمل',
+  'notactive'                         => 'لا يعمل',
+  'failed_title'                      => '😥😥😥',
+  'success_title'                     => '👍👍',
+  'no_ability'                        => 'لا تمتلك تلك الصلاحية  بلغ المدير المسئول',
+  'failed'                            => 'حدث خطأ وتم ابلاغ المدير برجاء المحاولة فى وقت لاحق ',
+  'are_you_sure_to_delete'            => 'هل أنت متاكد من الحذف',
+  'cancel'                            => 'إلغاء',
+  'confirm_delete'                    => 'نعم احذف الان!',
+  'forget_password'                   => 'نسيت كلمة المرور',
+  'reset_password'                    => 'إعادة تعيين كلمة المرور',
+  'remmber_me'                        => 'تذكرنى',
+
+
+  'profile'                           => 'الملف الشخصى',
+  'dashboard'                         => 'الرئيسيه',
+  'admin'                             => 'المديرين',
+  'role'                              => 'الصلاحيات',
+  'admin_set_active_1'                => 'تم تفعيل الأدمن بنجاح',
+  'admin_set_active_0'                => 'تم الغاء تفعيل الأدمن بنجاح',
+  'admin_get_active_1'                => 'مفعل',
+  'admin_get_active_0'                => 'غير مفعل',
+
+
+  'governorate'                       => 'المحافظه',
+  'city'                              => 'المدينه',
+  'user_name'                         => 'اسم المستخدم',
+  'password'                          => 'كلمة المرور',
+  'new_password'                      => 'كلمة المرور الجديده',
+  'old_password'                      => 'كلمة المرور السابقه',
+  'password_confirmation'             => 'تاكيد كلمة المرور',
+  'change_password'                   => 'تعديل كلمة المرور',
+
+  'fullname'                          => 'الأسم بالكامل',
+  'email'                             => 'البريد الالكترونى',
+  'phone'                             => 'رقم التليفون',
+  'other_phone'                       => 'رقم تليفون إضافى',
+  'address'                           => 'العنوان',
+
+  'status'                            => 'الحاله',
+  'role_id'                           => 'الصلاحيات',
+
+  'image'                             => 'صورة الملف الشخصى',
+  'choose_image'                      => 'إختار صورة',
+
+  'special_marque'                    => 'علامه مميزه',
+  'house_number'                      => 'رقم المنزل / العقار',
+  'door_number'                       => 'رقم الدور',
+  'shaka_number'                      => 'رقم الشقه',
+  'company_name'                      => 'أسم الشركه / ستور',
+
+  'facebook_page'                     => 'لينك صفحة الفيس',
+
+  'role_name'                         => 'أسم المجموعه',
+  'role_name_placeholder'             => 'مثال : كافة الصلاحيات',
+  'contract_type'                     => 'نوع التعاقد',
+  'daily'                             => 'يومى',
+  'monthly'                           => 'شهرى',
+  'activity'                          => 'النشاط',
+  'price_weight'                      => 'كيلو',
+  'price_bound'                       => 'جنيه',
+  'price_send_weight'                 => 'حتى وزن',
+  'price_send_weight_.5k'             => 'نصف كيلو',
+  'price_send_weight_1k'              => 'واحد كيلو',
+  'price_send_weight_2k'              => 'اتنين كيلو',
+  'price_send_weight_3k'              => 'ثلاثة كيلو',
+  'price_send_price'                  => 'سعر التوصيل',
+  'price_weight_addtion'              => 'الوزن الإضافى',
+  'price_price_addtion'               => 'السعر الإضافى',
+  'sender_info_title'                 => 'بيانات العميل /الراسل',
+  'sender_fullname'                   => 'اسم العميل ',
+  'reciver_info_title'                => 'بيانات المرسل إليه ',
+  'reciver_fullname'                  => 'اسم المرسل إليه',
+
+  'order_title'                       => 'الشحنه',
+  'order_info_title'                  => 'تفاصيل الشحنه',
+  'order_type'                        => 'اختار نوع الشحن',
+  'order_info'                        => 'وصف الشحنه',
+  'order_notes'                       => 'ملاحظات',
+
+  'order_weight'                      => 'وزن الشحنه',
+  'order_quantity'                    => 'عدد المنتجات',
+  'order_price'                       => 'قيمة الشحنه',
+  'order_charge_price'                => 'تكاليف الشحن',
+  'order_total_price'                 => 'إجمالى الشحنه',
+  'order_serial'                      => 'رقم البوليصه',
+  'order_charge'                      => 'تكلفة مصاريف الشحن',
+  'order_charge_sender'               => 'على الراسل',
+  'order_charge_reciver'              => 'على المرسل إليه',
+  'order_status'                      => 'حالة الشحنه',
+  'order_status_phone_from_customer'       => 'إتصال هاتفى للعميل',
+  'order_status_customer_store_in_company' => 'الإستلام من العميل ف مقر الشركه',
+
+  'admin_user_name'                   => 'اسم المستخدم',
+  'admin_password'                    => 'كلمة السر',
+  'order_total_weight'                => 'إجمالى وزن الشحنة',
+  'order_total_over_weight'           => 'إجمالى الوزن الزائد ',
+  'order_total_over_weight_price'     => 'تكاليف الوزن الزائد ',
+  'order_discount'                    => 'تقديم خصم للعميل',
+  'order_quantity_title'              => 'قطعه',
+
+  // 13/9/2020
+  'order_governorates_delivery'       => 'توصيل محافظات',
+  'order_next_day_delivery'           => 'توصيل تانى يوم',
+  'order_international_shipping'      => 'شحن دولي',
+  'order_packaging_service'           => 'خدمة التغليف',
+  'order_correspondents_service'      => 'خدمة المراسلين',
+  'order_send_transmitters_service'   => 'خدمة إرسال المرسلات',
+  'order_document_delivery_service'   => 'خدمة توصيل المستندات',
+  'order_same_day_delivery'           => 'توصيل في نفس اليوم',
+  //14/9/2020
+  'order_info_placeholder'            => 'أكتب وصفاً يدل على محتوى الشحنه',
+  'order_notes_placeholder'           => 'أكتب اى ملاحظه خاصه بالشحنه',
+  'order_weight_placeholder'          => 'اكتب الوزن بالكيلو',
+  'order_quantity_placeholder'        => 'اكتب عدداً صحيحاً',
+  'order_price_placeholder'           => 'أكتب المبلغ بالجنيه',
+  'order_discount_placeholder'        => 'أكتب المبلغ بالجنيه',
+  'order_user_can_open_order'         => 'السماح للعميل بفتح الشحنه',
+  'yes'                               => 'نعم',
+  'no'                                => 'لا',
+  //15/9/2020
+  'fullname_placholder'               => 'اسم العميل /الراسل',
+  'phone_placholder'                  => 'مثال: 01133445566',
+  'other_phone_placholder'            => 'مثال: 01033445500',
+  'address_placholder'                => 'مثال: شارع محى الدين ابو العز',
+  'special_marque_placholder'         => 'مثال: بجوار مسجد او مدرسة كذا',
+  'house_number_placholder'           => 'مثال: 123',
+  'door_number_placholder'            => 'مثال: 3',
+  'shaka_number_placholder'           => 'مثال: 5',
+
+  //16/9/2020
+  'place_price_not_found'             => 'لم تقم باضافة سعر الشحن لهذه المنطقه',
+
+  //17/9/2020
+  'delegate_active'                   => 'حالة المندوب',
+  'national_id'                       => 'الرقم القومى',
+  'national_image'                    => 'إرفاق صورة البطاقه',
+  'qualification'                     => 'المؤهل',
+  'social_status'                     => 'الحاله الأجتماعيه',
+
+  'social_status_single'              => 'أعزب',
+  'social_status_married'             => 'متزوج',
+  'social_status_divorce'             => 'مطلق',
+  'social_status_widower'             => 'ارمل',
+
+  'delegate_image'                    => 'الصوره الشخصيه',
+  'driveType'                         => 'نوع المركبه',
+  'driveType_motocycle'               => 'موتوسيكل',
+  'driveType_car'                     => 'سياره',
+  'driveType_trocycle'                => 'تروسيكل مغلق',
+  'driveColor'                        => 'لون المركبه',
+  'drivePlate_number'                 => 'رقم اللوحه',
+
+  'national_id_placeholder'           => 'مثال: 28127814021122',
+  'qualification_placeholder'         => 'مثال: دبلوم او بكالوريوس تجاره',
+  'social_status_placeholder'         => 'dd',
+  'driveColor_placeholder'            => 'مثال: اسود ، أحمر ، أبيض',
+  'driveplate_number_placeholder'     => 'مثال: س ص ع / 2030',
+  'address_placeholder'               => 'مثال: شارع محى الدين ابو العز',
+  'delegate_set_active_1'             => 'تم تفعيل المندوب بنجاح',
+  'delegate_set_active_0'             => 'تم الغاء تفعيل المندوب بنجاح',
+  'delegate_get_active_1'             => 'يعمل',
+  'delegate_get_active_0'             => 'لا يعمل',
+  'website_name'                      => 'سبيد',
+  'website_name_b'                    => 'للشحن السريع',
+  'login_title'                       => 'تسجيل الدخول لبدء الجلسه الخاصة بك',
+
+  'sellersouq_order_num'              => 'رقم بوليصة سوق',
+  'sellersouq_order_info'             => 'وصف البيك أب',
+  'sellersouq_order_quantity'         => 'عدد محتويات البيك أب',
+  'sellersouq_order_weight'           => 'وزن البيك أب',
+  'seller_add'                        => 'إضافة بيك أب جديد',
+  'order_number_badge'                => 'بيك أب رقم',
+  'facebook_register'                 => 'التسجيل بواسطة حساب فيس بوك',
+  'register'  => 'تسجيل',
+  // 23/9/2020
+
+  'order_status_all_orders'           => 'كل الشحنات',
+  'order_status_under_review'         => 'قيد المراجعه',
+  'order_status_under_preparation'    => 'تحت الاعداد',
+  'order_status_my_balance'           => 'رصيدى',
+  'order_status_ready_to_chip'        => 'جاهز للشحن',
+  'order_status_delivered'            => 'تم التوصيل',
+  'order_status_postpond'             => 'تم التاجيل',
+  'order_status_cancelld'             => 'تم الالغاء',
+
+  //24/9/2020
+  'more_info'                         => 'عرض المزيد',
+  'search_placeholder'                => 'ابحث عما تريد',
+  'reciver_show_title'                => 'بيانات العميل',
+  'order_show_title'                  => 'بيانات الشحنه',
+  'order_show_shipping'               => 'التكاليف',
+  'order_show_date'                   => 'التاريخ',
+  'order_user_can_open_order_0'       => 'لا',
+  'order_user_can_open_order_1'       => 'نعم',
+  'order_show_discount'               => 'قيمة الخصم',
+  'order_print_num'                   => 'رقم البوليصه',
+  'order_print_siteName'              => 'SPEED',
+  'order_print_city'                  => 'المنطقه',
+  'order_print_governorate'           => 'المدينه',
+  'order_print_date'                  => 'التاريخ',
+  'order_print_employee'              => 'بيانات الراسل',
+  'order_print_reciver'               => 'بيانات المرسل إليه',
+  'order_print_name'                  => 'الاسم',
+  'order_print_address'               => 'العنوان',
+  'order_print_phone'                 => 'التليفون',
+  'order_print_price'                 => 'القيمه',
+  'order_print_charge_price'          => 'قيمة الشحن',
+  'order_print_notes'                 => 'ملاحظات',
+  'order_print_danger_msg'            => 'الشركة غير مسؤوله عن محتويات الشحنه وتنتهي مسئوليتها بمجرد تسليمها للمرسل إليه',
+  'order_print_contact_msg'           => 'فى حالة الأستفسار أو الأستعلام يرجى التواصل مع خدمة العملاء على 01148630107 - 01098026159',
+
+  //06/10/2020
+  'customer_show_title'               => 'بيانات العميل',
+  'manager_reciver_title'             => 'بيانات المرسل إليه',
+  'customer_type_registered'          => 'كل العملاء المسجلين' ,
+  'customer_type_unRegistered'        => 'كل العملاء الغير مسجلين' ,
+  //08/10/2002
+  'order_print_door_num'              => 'الدور',
+  'order_print_shaka_number'          => 'شقه',
+  'order_print_notes'                 => 'ملاحظات',
+  'order_print_info'                  => 'وصف الاوردر',
+  'order_print_user_can_open_order_1' => 'السماح للعميل بفتح الشحنه',
+  'order_print_user_can_open_order_0' => 'عدم السماح للعميل بفتح الشحنه',
+  'order_print_charge_on'             => 'مصاريف الشحن',
+  'order_print_charge_on_sender'      => 'على الراسل',
+  'order_print_charge_on_reciver'     => 'على المرسل إليه',
+  'order_print_finished'              => 'خالص',
+  'order_print_get_price'             => 'تحصيل',
+  'order_print_other'                 => 'اخرى',
+  'order_print_website_name'          => 'سبيد للشحن السريع',
+  'order_print_website_name_en'       => 'SPEED',
+  'order_print_true'                  => '✔',
+  'order_print_false'                 => '❌',
+  'order_print_open_charge'           => 'فتح الشحنه',
+  'order_print_order_weight'          => 'وزن الشحنه',
+  'order_weight_kg'                   => 'كيلو جرام',
+  'order_special_marque'              => 'ع مميزه',
+  'modal_print'                       => 'طباعه',
+  'order_create_new_sender'           => 'عميل جديد',
+  'order_create_existing_sender'      => 'عميل موجود بالفعل',
+  'order_create_choose_customer'      => 'اختر العميل',
+  'order_create_new_reciver_manager'       => 'مرسل إليه جديد',
+  'order_create_existing_reciver_manager'  => 'مرسل إليه موجود بالفعل',
+  'order_create_choose_reciver_manager'    => 'اختر المرسل إليه',
+
+  'order_create_new_reciver_customer'      => 'عميل جديد',
+  'order_create_existing_reciver_customer' => 'عميل موجود بالفعل',
+  'order_create_choose_reciver_customer'   => 'اختر العميل',
+  'no_record'                              => 'لا يوجد',
+  'city_name_ar'                           => 'اسم المدينه بالعربى',
+  'city_name_en'                           => 'اسم المدينه بالانجليزى',
+  'show_records'                           => 'عرض',
+  'city_add'                               => 'إضافة مدينه جديده',
+  'option_view_style'                      => 'طريقة العرض',
+  'show_records_number'                    => 'عدد النتائج',
+  'save'                                   => 'حفظ  ',
+  'view_setting'                           => 'إعدادات العرض',
+  'nothing'                                => 'لا يوجد',
+  'id_not_found_title'                     => '👀👀',
+  'id_not_found'                           => 'لم نجد ما تريد عرضه حاول مرة اخرى فى وقت لاحق',
+  'order_print_price_contain_charge_price' => 'جنيه شامل مصاريف الشحن',
+
+  'order_edit_customer'                    => 'تعديل بيانات العميل',
+  'order_edit_reciver'                     => 'تعديل بيانات المرسل إليه',
+  'order_edit_order'                       => 'تعديل بيانات  الشحنه',
+  'order_edit_title'                       => 'الشحنه',
+  'order_edit_delegate_title'              => 'المندوب',
+  'edit_reciver_not_found_customer'        => 'بيانات العميل غير موجودة لدينا جرب واحد صحيح من فضلك',
+  'edit_reciver_not_for_curent_customer'   => 'هذا العميل لا يوجد ضمن قائمة العملاء الخاصة بك',
+  'edit_reciver_not_found_manager'         => 'بيانات المرسل إليه غير موجوده لدينا تأكد رجاءاً من البيانات ثم أعد المحاوله لاحقًا ',
+
+  'alert_add_order'                        => 'إضافة طلب جديد',
+  'alert_view_order'                       => 'عرض كافة الطلبات',
+  'no_recivers_for_customer'               => 'لا يوجد عملاء لهذا العميل',
+  'no_recivers_for_customer_message'       => 'لا يوجد عملاء لهذا العميل قم باختيار مرسل إليه جديد',
+  'close'                                  => 'إغلاق',
+  'telegram_add_order'                     => '#إضافة_طلب_جديد',
+  'telegram_date'                          => 'التاريخ',
+  'telegram_do'                            => 'قام',
+  'telegram_do_new_order'                  => 'بأضافة طلب جديد',
+  'telegram_order_info'                    =>'الطلب عبارة عن',
+  'telegram_details'                       =>'التفاصيل',
+  'telegram_review_order'                  =>'لمراجعة الطلب',
+  'telegram_click_here'                    =>'أضغط هنا',
+  'retry'                                  => 'إعادة المحاوله',
+  'alert_add_customer' => 'اضافة عميل جديد',
+  'alert_view_customer' => 'عرض كافة العملاء',
+  'alert_add_delegate' => 'اضافة مندوب جديد',
+  'alert_view_delegate' => 'عرض كافة المناديب',
+  'telegram_review_call_reciver' => 'الاتصال بالعميل',
+
+  'alert_add_price' => 'اضافة سعر جديد',
+  'alert_view_price' => 'عرض كافة الاسعار',
+  'alert_add_place' => 'اضافة منطقة جديدة',
+  'alert_view_place' => 'كافة المناطق',
+
+  'order_update_order'=> 'تحديث الشحنة',
+  'order_change_status' => 'تحديث حالة الشحنة',
+  'order_under_preparation_change' => 'تمت مراجعة الشحنة',
+  'order_cancelled_change' => 'الغاء الشحنة وحذفها',
+  'order_status_changed_successfuly' => 'تم تعديل الحالة بنجاح',
+  'order_status_changed_failed' => 'حدث خطا حاول مرة اخرى',
+  'Receipt_from_the_customer' => 'الاستلام من العميل',
+  'Receipt_order' => 'الاستلام',
+  'Receipt_from_the_customer_in_company'   => 'الأستلام من العميل ف مقر الشركه',
+  'Receipt_from_the_customer_by_delegate'  => 'الأستلام من العميل بواسطة مندوب  ',
+  'choose_delegate'                        => 'اختر المندوب',
+  'no_delegates'                           => 'لا يوجد مناديب مسجلة قم بالإضافة من هنا ',
+  'order_status_ready_to_receipt'          => 'جاهز للأستلام',
+
+  'color_under_review'                     => 'lightblue',
+  'color_under_preparation'                => 'info',
+  'color_ready_to_chip'                    => 'teal',
+  'color_delivered'                        => 'success',
+  'color_postpond'                         => 'purple',
+  'color_cancelld'                         => 'danger',
+  'color_ready_to_receipt'                 => 'olive',
+  'order_update'                           => 'تحديث',
+  'Receipt_order_status'                   => 'حالة الشحنة',
+  'Receipt_from_the_customer_done'         => 'تم استلام الشحنة من العميل',
+  'order_after_Receipt'                    => 'مسار الشحنة',
+  'go_to_storage'                          => 'العودة الى المخزن',
+  'go_to_customer'                         => 'الذهاب الى العميل',
+  'Delivery_to_the_customer'               => 'التوصيل الى العميل',
+  'Delivery_to_the_customer_done'          => 'تم التوصيل الى العميل',
+  'Delivery_to_the_customer_failed'        => 'تم رفض الاستلام',
+  'Delivery_cancelled_reason'              => 'سبب الرفض',
+  'Delivery_cancelled_reason_address_wrong'=> 'عنوان خاطئ',
+  'Delivery_cancelled_reason_customer_address_new' => 'عنوان جديد',
+  'Delivery_cancelled_reason_customer_not_found'   => 'عميل غير متوفر',
+];
---
+++ resources/lang/ar/website.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+  'title' => 'Connect4 Arab Pro',
+  'login' => [
+      'box_header' => 'سجل دخولك لمزيد من المتعة',
+  ],
+];
