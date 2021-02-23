30b5b3478a2fa16f217c75ff9012fda080f35bcc
Mando1987 <mandoooman@gmail.com>
2021-02-23 22:08:55 +0200

    added themes

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -9,6 +9,6 @@ class LoginController extends Controller
 {
     public function viewLoginPage()
     {
-       return view('backend.auth.login');
+        return view('backend.auth.login');
     }
 }
