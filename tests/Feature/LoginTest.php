4457d6fe00c4404a6675a7847eb8a42eb4c2d67d
Mando1987 <mandoooman@gmail.com>
2021-02-24 18:06:40 +0200

    added some file to testing

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -3,12 +3,17 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class LoginTest extends TestCase
 {
-
-    public function testBasicTest()
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
     {
         $response = $this->get('/');
 
